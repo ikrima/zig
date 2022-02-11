63a81a8fd96a652bbf96871dafff12370d824e06
ikrima <contact@ikrima.com>
2022-02-11 17:14:56 -0600

    add compiler shim

---
+++ zig_compiler_shim.zig
@@ -0,0 +1,9 @@
+// zig fmt: off
+const     std         = @import("std");
+pub const AstGen      = @import("src/AstGen.zig");
+pub const Compilation = @import("src/Compilation.zig");
+pub const Module      = @import("src/Module.zig");
+pub const Package     = @import("src/Package.zig");
+pub const Zir         = @import("src/Zir.zig");
+pub const print_zir   = @import("src/print_zir.zig");
+// zig fmt: on
