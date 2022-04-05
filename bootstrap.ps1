f82393573d5109410df726597572f0cb3a65098e
ikrima <contact@ikrima.com>
2022-04-05 12:54:23 -0500

    update bootstrap

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,85 +1,65 @@
-[CmdletBinding(SupportsShouldProcess = $true)]
+[CmdletBinding(SupportsShouldProcess)]
 param(
   # Attempt to build stage2
   [switch]$stage2,
   # Build single threaded version
   [switch]$singleThreaded
 )
-$ErrorActionPreference = "Stop"
+Import-Module PsUtil
 
-$ZigSrcDir = Resolve-Path $PSScriptRoot
-$ZigTarget = "x86_64-windows-gnu"
-$ZigDevKitName = "zig+llvm+lld+clang-$ZigTarget-0.9.1"
-$ZigDevKitPrefix = Resolve-Path "$ZigSrcDir/../$ZigDevKitName"
-$ZigDevKitURL = "https://ziglang.org/deps/$ZigDevKitName.zip"
+$ZigSrcDir       = ConvertTo-NormPath $PSScriptRoot
+$ZigTarget       = "x86_64-windows-gnu"
+$ZigDevKitName   = "zig+llvm+lld+clang-$ZigTarget-0.9.1"
+$ZigDevKitPrefix = ConvertTo-NormPath "$ZigSrcDir/../$ZigDevKitName"
+$ZigDevKitURL    = "https://ziglang.org/deps/$ZigDevKitName.zip"
+
+$devkit_exe      = ConvertTo-NormPath "$env:EDEV_ZIGDIR/zig.exe" # ConvertTo-NormPath "$ZigDevKitPrefix/bin/zig.exe"
+$stage1_dir      = "build-stage1"                               # ConvertTo-NormPath "build-stage1"
+$stage2_dir      = "build-stage2"                               # ConvertTo-NormPath "build-stage2"
+$stage1_exe      = "$stage1_dir/bin/zig.exe"                    # ConvertTo-NormPath "stage1/bin/zig.exe"
+$stage2_exe      = "$stage2_bld/bin/zig.exe"                    # ConvertTo-NormPath "stage2/bin/zig.exe"
 
 if (-not $stage2) {
   # using zig master, not devkit
-  $master_zigExe = Resolve-Path "$env:EDEV_ZIGDIR/zig.exe"
-  $stage0_BldArgs = @(
+  $stage0_bld_args = @(
     "build",
-    "--search-prefix", "$ZigDevKitPrefix",
+    "--search-prefix", $ZigDevKitPrefix,
+    "--prefix",        $stage1_dir,
     "-Dstage1",
     "-Domit-stage2",
-    "-Dstatic-llvm",
+    "-Dstatic-llvm", # "-Dstatic-llvm=false",
+    "-Duse-zig-libcxx",
+    "-Dtarget=$ZigTarget"
+    # "--zig-lib-dir", $ZigLibDir,
     # "-Drelease",
     # "-Dstrip",
-    "-Duse-zig-libcxx",
-    "-Dtarget=$ZigTarget"
   )
-  if ($singleThreaded) { $stage0_BldArgs += "-Dsingle-threaded" }
-  if ($PSCmdlet.ShouldProcess("$master_zigExe $($stage0_BldArgs -join " ")")) {
-    & $master_zigExe $stage0_BldArgs
-  }
+  if ($singleThreaded) { $stage0_bld_args += "-Dsingle-threaded" }
+  $stage1_bld_cmd = @(, $devkit_exe) + $stage0_bld_args -join " "
+  Write-Host $stage1_bld_cmd
+  if ($PSCmdlet.ShouldProcess($stage1_bld_cmd)) { & $devkit_exe $stage0_bld_args }
 }
-else {
+
+if ($stage2) {
   Write-Error "not working yet"
-  # master build.zig might be too new use devkits
-  if (-not (Test-Path "$ZigSrcDir/build_stage1.zig")) {
-    Copy-Item "$ZigSrcDir/ci/azure/build.zig" "$ZigSrcDir/build_stage1.zig";
-  }
 
-  $devkit_zigExe = Resolve-Path "$ZigDevKitPrefix/bin/zig.exe"
-
-  $stage1_BldDir = New-Item "$ZigSrcDir/build/stage1" -ItemType Directory -Force
-  $stage1_BldFile = Resolve-Path "$ZigSrcDir/build_stage1.zig"
-  $stage1_zigExe = "$stage1_BldDir/bin/zig.exe"
-  $stage1_BldArgs = @(
+  $stage2_bld_args = @(
     "build",
-    "--search-prefix", "$ZigDevKitPrefix",
-    "--prefix", "$stage1_BldDir",
-    "--build-file", "$stage1_BldFile",
-    "-Dstage1",
-    "-Dstatic-llvm",
-    "-Duse-zig-libcxx",
-    "-Dtarget=$ZigTarget",
-    "-Domit-stage2"
-    <# "-Drelease", "-Dstrip", #>
-  )
-  if ($PSCmdlet.ShouldProcess("$devkit_zigExe $($stage1_BldArgs -join " ")")) {
-    & "$devkit_zigExe" $stage1_BldArgs
-  }
-
-
-  $stage2_BldDir = New-Item "$ZigSrcDir/build/stage2" -ItemType Directory -Force
-  $stage2_BldFile = Resolve-Path "$ZigSrcDir/build.zig"
-  $stage2_Zig = "$stage2_BldDir/bin/zig.exe"
-  $stage2_BldArgs = @(
-    "build",
-    "--search-prefix", "$ZigDevKitPrefix",
-    "--prefix", "$stage2_BldDir",
-    "--build-file", "$stage2_BldFile",
+    "--search-prefix", $ZigDevKitPrefix,
+    "--prefix",        $stage2_dir,
     "-Denable-llvm",
-    "-Dstatic-llvm",
     "-Duse-zig-libcxx",
     "-Dtarget=$ZigTarget"
+    # "--zig-lib-dir",   $ZigLibDir,
   )
-  if ($PSCmdlet.ShouldProcess("$stage1_zigExe $($stage2_BldArgs -join " ")")) {
-    & "$stage1_zigExe" $stage2_BldArgs
-  }
+  $stage2_bld_cmd = @(, $stage1_exe) + $stage2_bld_args -join " "
+  Write-Host $stage2_bld_cmd
+  if ($PSCmdlet.ShouldProcess($stage2_bld_cmd)) { & $stage1_exe $stage2_bld_args }
 
-  # # Test
-  # #& "$ZigInstallDir\bin\zig.exe" test "..\test\behavior.zig" -fno-stage1 -fLLVM -I "..\test" 2>&1 #stage2 is omitted
-  # & "$ZigInstallDir\bin\zig.exe" build test-toolchain -Dskip-non-native -Dskip-stage2-tests 2>&1
-  # & "$ZigInstallDir\bin\zig.exe" build test-std -Dskip-non-native  2>&1
+  # Test
+  # & $stage2_exe build test-std -Dskip-release -Dskip-non-native  2>&1
+  # & $stage2_exe test "test/behavior.zig" -fno-stage1 -fLLVM -I "test"
+  # & $stage2_exe test "..\test\behavior.zig" -fno-stage1 -fLLVM -I "..\test" 2>&1 #stage2 is omitted
+  # & $stage2_exe build test-toolchain -Dskip-non-native -Dskip-stage2-tests 2>&1
+  # & $stage2_exe build test-std -Dskip-non-native  2>&1
 }
